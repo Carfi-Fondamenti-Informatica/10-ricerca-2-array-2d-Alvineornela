e2570e0c2e0d946c6e3ccc8a2c173789ecc71f71
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-06-08 23:01:15 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -32,5 +32,17 @@ void inserimento(char nome[], int n){
 }
 
 int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]){
+ int i = -1;
 
+    for(int j = 0; j<5; j++){
+        for(int k = 0; k<25; k++){
+            if(nomi[j][k] == nome[k]){
+                if(cognomi[j][k] == cognome[k]) {
+                    i++;
+                }
+            }
+        }
+
+    }
+    return i;
 }
