3c32eac274b7c16d8ae56e149f8a714f3c557e93
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-06-08 22:40:09 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+void init(char mat[], int n){
+    for(int i = 0; i < n; i++){
+        mat[i]=' ';
+    }
+}
+
+void init(char mat[5][15]){
+    for(int i = 0; i < 5; i++){
+        init(mat[i],15);
+    }
+}
+
+void inserimento(char nomi[5][15], char cognomi[5][15]){
+    for(int i = 0; i < 5; i++){
+        inserimento(nomi[i],15);
+        inserimento(cognomi[i],15);
+    }
+}
+
+void inserimento(char nome[], int n){
+    char cpynome[n];
+    init(cpynome,n);
+    cin>>cpynome;
+    for(int i = 0; i < n; i++){
+        nome[i] = cpynome[i];
+    }
+}
+
+int ricerca(char[5][15], char cognomi[5][15], char[15], char cognome[15]){
+
+}
